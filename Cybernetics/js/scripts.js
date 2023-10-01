02779048142f6406a58194e74adb88e9c35e412b
CoderFrancis <mfcollegeacc@gmail.com>
2023-10-01 17:19:43 +0530

    20231001 @1720

--- Cybernetics/js/scripts.js
+++ Cybernetics/js/scripts.js
@@ -3,14 +3,12 @@ let slideIndex = 0;
 function showSlide(n) {
     let slides = document.getElementsByClassName("slide");
 
-    // Wrap around if index goes out of bounds
     if (n >= slides.length) {
         slideIndex = 0;
     } else if (n < 0) {
         slideIndex = slides.length - 1;
     }
 
-    // Translate the slides to show the current one
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.transform = `translateX(-${slideIndex * 100}%)`;
     }
@@ -22,22 +20,25 @@ function moveSlide(n) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Add images to the gallery dynamically
-    const gallery = document.getElementById('gallery');
-    const images = ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg']; // add image file names
+    const slideshowContainer = document.querySelector('.slideshow-container');
+    const images = ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg'];
 
-    images.forEach(img => {
-        const imageElement = document.createElement('img');
-        imageElement.src = img;
-        imageElement.style.maxWidth = '300px';
-        imageElement.style.margin = '10px';
-        gallery.appendChild(imageElement);
+    images.forEach(imgSrc => {
+        // Create a new slide div for each image
+        const slideDiv = document.createElement('div');
+        slideDiv.className = "slide fade";
+
+        const imgElement = document.createElement('img');
+        imgElement.src = imgSrc;
+        imgElement.alt = "Description for " + imgSrc; // Adjust as per your need
+
+        // Append the img to the slide div and the slide div to the slideshow container
+        slideDiv.appendChild(imgElement);
+        slideshowContainer.appendChild(slideDiv);
     });
 
-    // Show the first slide
+    // Show the first slide and start the slideshow timer
     showSlide(slideIndex);
-
-    // Automatic slideshow - change image every 5 seconds
     setInterval(function() {
         moveSlide(1);
     }, 5000);
