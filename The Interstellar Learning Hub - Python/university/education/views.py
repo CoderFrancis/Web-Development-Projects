8044b61c370005bc9f1ac94803753e03f9f2ed1b
CoderFrancis <mfcollegeacc@gmail.com>
2024-01-24 19:41:17 +0530

    20240124 @1940

--- The Interstellar Learning Hub - Python/university/education/views.py
+++ The Interstellar Learning Hub - Python/university/education/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Student, Professor
 
-# Create your views here.
+
+def student_list(request):
+    students = Student.objects.all()  # Retrieve all students from the database
+    return render(request, 'education/student_list.html', {'students': students})
+
+
+def professor_list(request):
+    professors = Professor.objects.all()  # Retrieve all professors from the database
+    return render(request, 'education/professor_list.html', {'professors': professors})
